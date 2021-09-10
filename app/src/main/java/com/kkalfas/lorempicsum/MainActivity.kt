04516365c85fe36910aa81642775810b9f074f7a
Kamil Kalfas <kamilkalfas@gmail.com>
2021-09-10 16:45:46 +0200

    Added back navigation for layout Added CoreFragment.kt

--- app/src/main/java/com/kkalfas/lorempicsum/MainActivity.kt
+++ app/src/main/java/com/kkalfas/lorempicsum/MainActivity.kt
@@ -2,10 +2,16 @@ package com.kkalfas.lorempicsum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kkalfas.lorempicsum.welcome.WelcomeFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.main_activity)
+        supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, WelcomeFragment())
+                .commit()
     }
 }
---
+++ app/src/main/java/com/kkalfas/lorempicsum/welcome/signup/SignUpFragment.kt
@@ -0,0 +1,20 @@
+package com.kkalfas.lorempicsum.welcome.signup
+
+import android.os.Bundle
+import android.view.View
+import com.kkalfas.lorempicsum.R
+import com.kkalfas.lorempicsum.core.CoreFragment
+import com.kkalfas.lorempicsum.databinding.SignupFragmentBinding
+
+internal class SignUpFragment : CoreFragment<SignupFragmentBinding>(R.layout.signup_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding.signupToolbar) {
+            setNavigationIcon(R.drawable.ic_nav_back)
+            setNavigationOnClickListener {
+                // use navigation to go back to welcome_fragment
+            }
+        }
+    }
+}
